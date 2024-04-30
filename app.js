a99f83faffc0a06a535499d3f49600b602c81c24
priyanka-1994 <priyak06111994@gmail.com>
2024-04-30 13:35:14 +0530

    Updating app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hiii,I am trying to deploy nodejs application..This is my first project..!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
