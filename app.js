9a1ddda0f16b7eef6f517f2ef648d249693430f0
priyanka-1994 <priyak06111994@gmail.com>
2024-04-30 17:32:43 +0530

    changing message for manual approve

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hiii,I am trying to deploy nodejs application..This is my first project..!'));
+app.get('/', (req, res) => res.send('Hiii,I am trying to deploy nodejs application..This is my first project..! and I have done it...'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
